8ad87040e5582942a1345dc95885e21eaead00a1
Stefanos Kalogerakis <skalogerakis97@gmail.com>
2021-12-21 11:41:57 +0200

    Added some questions for clarification

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ onto = get_ontology("http://skalerakis.org/onto.owl")
 '''
     1) Satellite is equivalent to Artificial Satellite or Natural Satellite.
 '''
-
-
 class Satellite(Thing):
     namespace = onto
 
@@ -66,17 +64,26 @@ Satellite.equivalent_to.append(Or([ArtificialSatellite, NaturalSatellite]))
 # class isSatelliteOf(orbitsAround):
 #     inverse = hasForSatellite
 
-
-class CelestialBody(Thing):
+'''
+    hasCenterOfMass is Functional
+    Added direct definition here. The documentation states that we can ommit 
+'''
+#TODO ask about functional, not directly defined here
+class hasCenterOfMass(ObjectProperty, FunctionalProperty):
     namespace = onto
 
 
 class Galaxy(Thing):
     namespace = onto
+    is_a = [ hasCenterOfMass.max(1) ]
+#     is_a = [ hasCenterOfMass.some(Galaxy) ] instead maybe that??? TODO
 
 
+########################################################################################
 
 
+class CelestialBody(Thing):
+    namespace = onto
 
 class observes(ObjectProperty):
     namespace = onto
@@ -85,14 +92,23 @@ class observes(ObjectProperty):
 
 
 # 5) A space telescope is a celestial body that observes other celestial body
-class SpaceTelescope(CelestialBody):
+class SpaceTelescope(CelestialBody):    # TODO Do we need to define both in the parenthessis and in the is_a
     namespace = onto
     is_a = [CelestialBody & observes.some(CelestialBody)]
 
 
-# class orbitsAround():
-#     namespace = onto
+class orbitsAround(ObjectProperty):
+    namespace = onto
+    domain=[CelestialBody]
+    range = [Thing]
 
+# class Star(Thing):
+#     namespace = onto
+#
+#  Todo also ask about that. orbitsAround.Star vs orbitsAround.T
+# class Dwarf(CelestialBody):
+#     namespace = onto
+#     is_a = [CelestialBody & orbitsAround.some(Thing)]
 
 class Universe(Thing):
     namespace = onto
@@ -111,7 +127,7 @@ class locatedIn(ObjectProperty):
     inverse_property = hasMember    # 5) locatedIn property is the inverse of hasMember. NOTE: Don't need to add in the other function, owlready2 applies it on its own. Will this create an issue??
 
 # 6) Universe has at least one member that is a Galaxy
-Universe.is_a.append(hasMember.min(1, Galaxy))
+Universe.is_a.append(hasMember.min(1, Galaxy))      #TODO also similar with max cardinality
 
 # TODO all the Thing Concepts must be disjoint
 if __name__ == '__main__':
